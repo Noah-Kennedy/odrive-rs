d3d059a13605bb286c466f09b2e270115405e11d
noah <nomaxx117@gmail.com>
2019-11-04 23:13:19 -0600

    Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+/// The `commands` module contains the ODrive structure, which is used to interact with the ODrive
+/// protocol.
 pub mod commands;
+
+/// The `enumerations` module contains enums and constants related to different properties and
+/// errors.
 pub mod enumerations;
