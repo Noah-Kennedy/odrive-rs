7fcfceb13ffdbeeb3c5566aae3762dd823c7800d
noah <nomaxx117@gmail.com>
2019-11-07 18:05:03 -0600

    Added tests for motor configuration

---
+++ src/commands/command_tests/motor_tests.rs
@@ -0,0 +1,33 @@
+use super::*;
+
+#[test]
+fn test_set_pole_pairs() {
+    let mut odrive = init_odrive();
+    odrive.set_motor_pole_pairs(Axis::Zero, 25).unwrap();
+    assert_eq!(b"w axis0.motor.config.pole_pairs 25\n".to_vec(), odrive.io_stream.write_buffer);
+    assert!(odrive.io_stream.flushed)
+}
+
+#[test]
+fn test_set_resistance_calibration_max_voltage() {
+    let mut odrive = init_odrive();
+    odrive.set_motor_resistance_calib_max_voltage(Axis::Zero, 25.0).unwrap();
+    assert_eq!(b"w axis0.motor.config.resistance_calib_max_voltage 25\n".to_vec(), odrive.io_stream.write_buffer);
+    assert!(odrive.io_stream.flushed)
+}
+
+#[test]
+fn test_set_requested_current_range() {
+    let mut odrive = init_odrive();
+    odrive.set_motor_requested_current_range(Axis::Zero, 25.0).unwrap();
+    assert_eq!(b"w axis0.motor.config.requested_current_range 25\n".to_vec(), odrive.io_stream.write_buffer);
+    assert!(odrive.io_stream.flushed)
+}
+
+#[test]
+fn test_set_current_control_bandwidth() {
+    let mut odrive = init_odrive();
+    odrive.set_motor_current_control_bandwidth(Axis::Zero, 25.0).unwrap();
+    assert_eq!(b"w axis0.motor.config.current_control_bandwidth 25\n".to_vec(), odrive.io_stream.write_buffer);
+    assert!(odrive.io_stream.flushed)
+}
